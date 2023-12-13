068885a8d37d3b2b766b0d92bdf9d8bca976a334
aileen <leenybobini425@gmail.com>
2023-12-13 15:51:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,28 +11,28 @@ function draw() {
 textSize(14);
     
 //randomly chosen prepositon word
-    var prep = ['behind', 'beyond', 'outside', 'past', 'around', 'toward', 'above', 'within'];
+    var prep = ["behind", "beyond", "outside", "past", "around", "toward", "above", "within"];
     var choosePrep = random(prep);
     fill(random(140, 219), 187, 211);
     text(choosePrep, 100, random(100, 500));
     textSize(14);
 
 //randomly chosen place/location word
-    var place = ['the apple tree,', 'the server farms,', 'the rivers edge,', 'the outlet mall,', 'the high school parking lot,', 'the gas station,', 'the lovers leap,', 'the pharmacy,', 'the amusement park,'];
+    var place = ["the apple tree,", "the server farms,", "the river's edge,", "the outlet mall,", "the high school parking lot,", "the gas station,", "the lover's leap,", "the pharmacy,", "the amusement park,"];
     var choosePlace = random(place);
     fill(random(140, 219), 187, 211);
     text(choosePlace, 400, random(100, 500));
     textSize(14);
 
 //randomly chosen noun
-    var noun = ['circuits', 'willows', 'fingers', 'empty cans', 'wires', 'monitors', 'cicadas', 'alarm clocks'];
+    var noun = ["circuits", "willows", "fingers", "empty cans", "wires", "monitors", "cicadas", "alarm clocks"];
     var chooseNoun = random(noun);
     fill(random(140, 219), 187, 211);
     text(chooseNoun, 700, random(100, 500));
     textSize(14);
 
 //randomly chosen verb
-    var verb = ['hum', 'scream', 'mutter', 'buzz', 'beep', 'holler', 'flutter', 'whisper'];
+    var verb = ["hum", "scream", "mutter", "buzz", "beep", "holler", "flutter", "whisper"];
     var chooseVerb = random(verb);
     fill(random(140, 219), 187, 211);
     text(chooseVerb, 1000, random(100, 500));
